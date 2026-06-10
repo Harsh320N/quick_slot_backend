96f847fde783b12e666ee59a3487277b2289e431
Harsh320N <harshnavapara320@gmail.com>
2026-06-10 21:16:46 +0530

    - uickSlot API listening

--- server/src/config.js
+++ server/src/config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   port: process.env.PORT || 3000,
+  host: process.env.HOST || '0.0.0.0',
   dbPath: process.env.DB_PATH || path.join(__dirname, '..', 'data', 'quickslot.db'),
-  // venues run hourly slots from 6 AM to 10 PM
   openHour: 6,
   closeHour: 22,
 };
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,22 @@
+const os = require('os');
 const app = require('./app');
-const { port } = require('./config');
+const { port, host } = require('./config');
 
-app.listen(port, () => {
-  console.log(`QuickSlot API listening on http://localhost:${port}`);
+function lanAddresses() {
+  const nets = os.networkInterfaces();
+  const ips = [];
+  for (const iface of Object.values(nets)) {
+    for (const net of iface) {
+      if (net.family === 'IPv4' && !net.internal) ips.push(net.address);
+    }
+  }
+  return ips;
+}
+
+app.listen(port, host, () => {
+  console.log(`QuickSlot API listening on port ${port}`);
+  console.log(`  local:   http://localhost:${port}`);
+  for (const ip of lanAddresses()) {
+    console.log(`  network: http://${ip}:${port}   <- use this from a phone`);
+  }
 });
